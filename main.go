bfa0d9eb464792ab0197419123022a1747c19a9c
Jeff Stein <jeffstein@gmail.com>
2026-06-30 10:48:36 -0600

    test: cover root cli entrypoint

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"os"
 	_ "time/tzdata"
 
@@ -10,8 +11,16 @@ import (
 )
 
 func main() {
-	if err := icsmcp.NewRootCommand().ExecuteContext(context.Background()); err != nil {
+	if err := executeCommand(os.Args[1:], os.Stdout, os.Stderr); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+}
+
+func executeCommand(args []string, stdout io.Writer, stderr io.Writer) error {
+	cmd := icsmcp.NewRootCommand()
+	cmd.SetArgs(args)
+	cmd.SetOut(stdout)
+	cmd.SetErr(stderr)
+	return cmd.ExecuteContext(context.Background())
 }
